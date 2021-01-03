3a549f5c62ec801148d1804c29ae0254a6b2e548
mousto16 <mbouombouomousto@gmail.com>
2021-01-03 08:59:03 +0100

    Remove product to cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -54,9 +54,14 @@ class ProductController extends Controller
         $products=DB::table('cart')
         ->join('products','cart.product_id','=','product_id')
         ->where('cart.user_id',$userId)
-        ->select('products.*')
+        ->select('products.*', 'cart.id as cart_id')
         ->get();
         
         return view('cartlist', ['products'=>$products]);
+    }
+
+    function removeCart($id){
+        Cart::destroy($id);
+        return redirect('cartlist');
     }
 }
